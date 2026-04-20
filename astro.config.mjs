966fd72d5510e448dbfcaba14ba0835abaf7486c
肖润龙 <xrlfreedom@gmail.com>
2026-04-20 20:40:28 +0800

    fix: total removal of sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://blog.xrlfreedom.top',
